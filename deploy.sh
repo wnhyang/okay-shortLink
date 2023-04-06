4eaf9d8307ed254b3f01f79de17075d955f34180
wnhyang <wnhyang@qq.com>
2023-04-06 09:31:24 +0800

    部署脚本去掉ip

--- deploy.sh
+++ deploy.sh
@@ -62,7 +62,7 @@ start_app() {
     docker run -v /home/app/logs:/home/app/logs \
                -p ${APP_PORT}:${APP_PORT} \
                -p 9999:9999 \
-               --add-host nacos:123.60.165.53 \
+               --add-host nacos:ip \
                --name ${APP_NAME} \
                -d ${DOCKER_URL}
      CONTAINER_ID=`docker ps -a | grep -v "grep" | grep ${APP_NAME} | awk -F: '{print $1}' | awk  '{print $1}' | head -n 1`
